e516b00898de6620177c1c44c690a842a2e17846
sneha390 <sneha39yadav@gmail.com>
2025-02-13 23:45:05 +0530

    Finding Duplicates

---
+++ array/medium/dublicate.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> findDuplicates(vector<int>& arr) {
+    unordered_map<int, int> freq;
+    vector<int> ans;
+
+    for (int num : arr) {
+        freq[num]++;
+    }
+
+    for (auto it : freq) {
+        if (it.second > 1) {
+            ans.push_back(it.first);
+        }
+    }
+    
+    return ans;
+}
+
+int main() {
+    vector<int> arr = {4, 3, 2, 7, 8, 2, 3, 1}; // Example input
+
+    vector<int> duplicates = findDuplicates(arr);
+
+    cout << "Duplicate elements are: ";
+    for (int num : duplicates) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
